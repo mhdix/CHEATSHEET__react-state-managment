66118fc9c544e778262de31b5ac405d14e139a1c
mhdix <domanloomu@gmail.com>
2026-02-23 22:18:54 +0330

    feature: start project

---
+++ src/components/Header.jsx
@@ -0,0 +1,125 @@
+import React, { useRef, useState } from "react";
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi";
+import { MdLocationOn } from "react-icons/md";
+import useOutsideClick from "../utils/useOutsideClick";
+
+const Header = () => {
+  const [destination, setDestination] = useState("");
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+  const [date, setDate] = useState([
+    startDate,
+    endDate,
+    key: "traveling-date-range"
+  ])
+  const [openDate, setOpenDate] = useState(false);
+  
+  const handleOptions = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "inc" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
+  return (
+    <div className="headerSearch">
+      <div className="headerSearchItem">
+        <MdLocationOn className="headerIcon locationIcon" />
+        <input
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+          type="text"
+          placeholder="where to go?"
+          className="headerSearchInput"
+          name="destination"
+          id="destination"
+        />
+        <span className="seperator"></span>
+      </div>
+      <div className="headerSearchItem">
+        <HiCalendar className="headerIcon dateIcon" />
+        <div className="dateDropDown" onClick={()=> setOpenDate(!openDate)}>2023/06/01</div>
+        <span className="seperator"></span>
+        {openDate }
+      </div>
+      <div className="headerSearchItem">
+        <div id="optionDropDown" onClick={() => setOpenOptions(!openOptions)}>
+          {options.adult} adult &nbsp;&bull;&nbsp; {options.children} children
+          &nbsp;&bull;&nbsp; 2 room
+          <span className="seperator"></span>
+        </div>
+        {openOptions && (
+          <GuestOptionList
+            setOpenOptions={setOpenOptions}
+            handleOptions={handleOptions}
+            options={options}
+          />
+        )}
+      </div>
+
+      <div className="headerSearchItem">
+        <button className="headerSearchBtn">
+          <HiSearch className="headerIcon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
+function GuestOptionList({ options, handleOptions, setOpenOptions }) {
+  const optionRef = useRef();
+
+  useOutsideClick(optionRef, "optionDropDown", () => setOpenOptions(false));
+  return (
+    <div className="guestOptions" ref={optionRef}>
+      <OptionItem
+        handleOptions={handleOptions}
+        type="adult"
+        options={options}
+        minLimit={1}
+      />
+      <OptionItem
+        handleOptions={handleOptions}
+        type="children"
+        options={options}
+        minLimit={0}
+      />
+      <OptionItem
+        handleOptions={handleOptions}
+        type="room"
+        options={options}
+        minLimit={1}
+      />
+    </div>
+  );
+}
+function OptionItem({ type, options, minLimit, handleOptions }) {
+  return (
+    <div className="guestOptionItem">
+      <span className="optionText">{type}</span>
+      <div className="optionCounter">
+        <button
+          onClick={() => handleOptions(type, "dec")}
+          className="optionCounterBtn"
+          disabled={options[type] <= minLimit}
+        >
+          <HiMinus className="icon" />
+        </button>
+        <span className="optionCounterNumber">{options[type]}</span>
+        <button
+          className="optionCounterBtn"
+          onClick={() => handleOptions(type, "inc")}
+        >
+          <HiPlus className="icon" />
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/file-Header/Header.jsx
@@ -0,0 +1,127 @@
+import { MdLocationOn, MdLogout } from "react-icons/md";
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi";
+import { useRef } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <NavLink to="/bookmark">Bookmarks</NavLink>
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            value=""
+            type="text"
+            placeholder="where to go?"
+            className="headerSearchInput"
+            name="destination"
+            id="destination"
+          />
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          {/* <div onClick={() => setOpenDate(!openDate)} className="dateDropDown">
+            {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+              date[0].endDate,
+              "MM/dd/yyyy",
+            )}`}
+          </div> */}
+          {/* <DateRange
+            onChange={(item) => setDate([item.selection])}
+            ranges={'date'}
+            className="date"
+            minDate={new Date()}
+            moveRangeOnFirstSelection={true}
+          /> */}
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          {/* <div id="optionDropDown" onClick={() => setOpenOptions(!openOptions)}>
+            {options.adult} adult &nbsp;&bull;&nbsp; {options.children} children
+            &nbsp;&bull;&nbsp;
+            {options.room} room
+          </div>
+          {openOptions && (
+            <GuestOptionList
+              setOpenOptions={setOpenOptions}
+              handleOptions={handleOptions}
+              options={options}
+            />
+          )} */}
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
+      </div>
+      <User />
+    </div>
+  );
+}
+export default Header;
+
+function GuestOptionList({ options, handleOptions, setOpenOptions }) {
+  const optionsRef = useRef();
+  return (
+    <div className="guestOptions" ref={optionsRef}>
+      {/* <OptionItem
+        handleOptions={handleOptions}
+        type="adult"
+        options={options}
+        minLimit={1}
+      />
+      <OptionItem
+        handleOptions={handleOptions}
+        type="children"
+        options={options}
+        minLimit={0}
+      />
+      <OptionItem
+        handleOptions={handleOptions}
+        type="room"
+        options={options}
+        minLimit={1}
+      /> */}
+    </div>
+  );
+}
+
+function OptionItem({ options, type, minLimit, handleOptions }) {
+  return (
+    <div className="guestOptionItem">
+      <span className="optionText">{'type'}</span>
+      <div className="optionCounter">
+        <button className="optionCounterBtn">
+          <HiMinus className="icon" />
+        </button>
+        <span className="optionCounterNumber">{'options[type]'}</span>
+        <button
+          className="optionCounterBtn"
+        >
+          <HiPlus className="icon" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function User() {
+  return (
+    <div>
+      <div>
+        <strong>{"user.name"}</strong>
+        <button>
+          &nbsp; <MdLogout className="logout icon" />
+        </button>
+      </div>
+      <NavLink to="/login">login</NavLink>
+    </div>
+  );
+}
