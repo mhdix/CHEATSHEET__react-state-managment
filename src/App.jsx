59ade5ef18bb0ac317b7219c1b78c96929eaee88
mhdix <domanloomu@gmail.com>
2026-02-24 19:26:45 +0330

    feature: nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import LocationList from "./components/LocationList";
 import { Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout/AppLayout";
 function App() {
   return (
     <div>
@@ -10,6 +11,10 @@ function App() {
       <Toaster />
       <Routes>
         <Route path="/" element={<LocationList />} />
+        <Route path="/hotels" element={<AppLayout />}>
+          <Route index element={<div>Hotels</div>} />
+          <Route path=":id" element={<div>single hotels</div>} />
+        </Route>
       </Routes>
       {/* <LocationList  /> */}
     </div>
@@ -19,3 +24,4 @@ function App() {
 export default App;
 
 //? video 105
+//? video 109
